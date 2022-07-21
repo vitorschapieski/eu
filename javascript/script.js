c3bdd0fab164998ada9b2bbf5f6e55dc63573344
vitorschapieski <fullstackschapieski@gmail.com>
2022-07-21 00:09:18 -0300

    ::

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,10 @@
-let fechar = document.querySelector('.fechar')
-let photos = document.querySelector('.photos')
-let modal = document.querySelector('.modal')
-let jaja = document.querySelector('.jaja')
 let skillButton = document.querySelector('.skills')
 let skillPage = document.querySelector('.skill')
 let pagePosts = document.querySelector('.Posts')
 
 function toggle(){
     skillPage.classList.toggle('ativar');
-    pagePosts.classList.toggle('ativar')
+    pagePosts.classList.toggle('ativar');
 }
 
 skillButton.addEventListener('click', toggle);
