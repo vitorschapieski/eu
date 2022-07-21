b0131fe0cba63619ea8948a15d535afba49b9a4a
vitorschapieski <fullstackschapieski@gmail.com>
2022-07-20 22:51:18 -0300

    :)

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 let fechar = document.querySelector('.fechar')
 let photos = document.querySelector('.photos')
 let modal = document.querySelector('.modal')
+let jaja = document.querySelector('.jaja')
 
 function toggle(){
     modal.classList.toggle('ativar');
